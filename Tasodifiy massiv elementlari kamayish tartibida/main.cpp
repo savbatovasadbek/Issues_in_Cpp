6426dd8668f7b3295d9b66f56aaad3cf2f198acf
SamandarTuraev <129301048+SamandarTuraev@users.noreply.github.com>
2023-04-06 14:07:54 +0500

    Update main.cpp
    Bu boshqa masalaga o'zgartirildi. Savol: "Berilgan massivdagi tub sonlar sonini topish?"

--- Tasodifiy massiv elementlari kamayish tartibida/main.cpp
+++ Tasodifiy massiv elementlari kamayish tartibida/main.cpp
@@ -1,9 +1,55 @@
-#include <iostream>
+ #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+ int main()
+ {
+    int num, soni = 0, sum = 0;
+    cout << "Massiv elementlari sonini kiriting: ";
+    cin >> num;
+
+    if (num > 0) {
+        int arr[num];
+
+        for (int i = 0; i < num; i++) {
+            cout << "arr[" << i << "] = ";
+            cin >> arr[i];
+        }
+
+        cout << "==============================================\n";
+        cout << "Berilgan massivdagi tub sonlar: \n";
+        cout << "==============================================\n";
+
+        for (int i = 0; i < num; i++) {
+
+            if (arr[i] > 0) {
+                for (int j = 1; j <= arr[i]; j++) {
+                    if (arr[i] % j == 0) {
+                        sum++;
+                    }
+                }
+                if (sum == 2) {
+                    cout << "arr[" << i << "] = " << arr[i] << endl;
+                    soni++;
+                }
+                sum = 0;
+            }
+        }
+
+        if (soni == 0) {
+            cout << "==============================================\n";
+            cout << "Tub sonlar mavjud emas! :(\n";
+            cout << "==============================================\n";
+        } else {
+            cout << "==============================================\n";
+            cout << "Tub sonlar " << soni << " ta.\n";
+            cout << "==============================================\n";
+        }
+    } else {
+        cout << "==============================================\n";
+        cout << "Massiv elementlari soni xato kiritildi!" << endl;
+        cout << "==============================================\n";
+    }
+
     return 0;
-}
+ }
